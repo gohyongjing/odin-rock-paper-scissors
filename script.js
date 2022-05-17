0fcef569d1d96d1bd8d1266585ce70911c3a0b88
Yong Jing <gohyongjing@gmail.com>
2022-05-17 15:25:07 +0800

    Update game to use alert and handle invalid input

--- script.js
+++ script.js
@@ -36,6 +36,9 @@ function getWinner(playerChoice, computerChoice) {
 }
 
 function getGameResult(playerChoice, computerChoice) {
+    if (playerChoice == null || playerChoice.length == 0) {
+        return 'Uh-oh! Round skipped!'
+    }
     playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1).toLowerCase();
     if (playerChoice !== 'Rock' && playerChoice !== 'Paper' && playerChoice !== 'Scissors') {
         return 'Uh-oh! You can only pick rock or paper or scissors!';
@@ -47,17 +50,22 @@ function getGameResult(playerChoice, computerChoice) {
     } else if (winner === 'Computer') {
         return `You lose! ${computerChoice} beats ${playerChoice}`;
     } else if (winner === 'None') {
-        return `It's a draw! Everyone picked ${computerChoice}`;
+        return `It's a draw! We both picked ${computerChoice}`;
     }
 }
 
+function playRound() {
+    let playerChoice = prompt('Pick your choice! (rock / paper / scissors)');
+    let result = getGameResult(playerChoice, computerPlay());
+    alert(result);
+}
+
 function game() {
-    console.log("Hello! Welcome to rock-paper-scissors");
+    alert("Hello! Welcome to rock-paper-scissors! Let's play 5 rounds!");
     for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt('Pick your choice! (rock / paper / scissors');
-        let result = getGameResult(playerChoice, computerPlay());
-        console.log(result);
+        playRound();
     }
+    alert('Hope you had fun!')
 }
 
 game();
